1853f1cf3f9788cf3a60fbbfd2c9af3de1f54bcc
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2024-12-06 09:13:54 -0300

    feat: create Address entity

---
+++ src/main/java/com/employee_management_jsf/domain/model/Address.java
@@ -0,0 +1,23 @@
+package com.employee_management_jsf.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+}
