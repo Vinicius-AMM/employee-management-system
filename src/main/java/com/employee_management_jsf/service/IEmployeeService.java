c51b98f7bf3dbc219d3675b812cbe64a9fa538e7
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2024-12-06 09:23:44 -0300

    feat: create IEmployeeService interface

---
+++ src/main/java/com/employee_management_jsf/service/IEmployeeService.java
@@ -0,0 +1,7 @@
+package com.employee_management_jsf.service;
+
+
+import com.employee_management_jsf.domain.model.Employee;
+
+public interface IEmployeeService extends ICrudService<Employee, String> {
+}
